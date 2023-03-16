7996fdf56493f49f2dbd01ed2d67e2a90c7bc659
Milind452 <mohapatra.milind@gmail.com>
2023-03-16 21:43:08 +0530

    Add category query

--- pages/api/graphql/index.js
+++ pages/api/graphql/index.js
@@ -8,10 +8,16 @@ const typeDefs = /* GraphQL */ `
         title: String!
         thumbnail: String!
         price: Float
+        category: Category
+    }
+    type Category {
+        id: Int!
+        title: String!
     }
     type Query {
         product: Product
         products(limit: Int): [Product]
+        categories: [Category]
     }
 `;
 
