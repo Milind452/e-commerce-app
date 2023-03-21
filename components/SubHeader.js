60166176dc5a66d0edebcfb054348fab813f7498
Milind452 <mohapatra.milind@gmail.com>
2023-03-21 10:43:14 +0530

    Use cart button instead of button in header

--- components/SubHeader.js
+++ components/SubHeader.js
@@ -1,37 +1,34 @@
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import Button from './Button';
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import CartButton from "./CartButton";
 
 const SubHeaderWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background: cornflowerBlue;
-  padding: 5px 10px;
+    display: flex;
+    justify-content: space-between;
+    background: cornflowerBlue;
+    padding: 5px 10px;
 `;
 
 const Title = styled.h2`
-  text-align: center;
-  flex-basis: 60%;
-  &:first-child {
-    margin-left: 20%;
-  }
-  &:last-child {
-    margin-right: 20%;
-  }
+    text-align: center;
+    flex-basis: 60%;
+    &:first-child {
+        margin-left: 20%;
+    }
+    &:last-child {
+        margin-right: 20%;
+    }
 `;
 
 function SubHeader({ title, goToCart = false }) {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <SubHeaderWrapper>
-      <Button onClick={() => router.back()}>{`< Go Back`}</Button>
-      <Title>{title}</Title>
-      {goToCart && (
-        <Button onClick={() => router.push('/cart')}>Cart (0)</Button>
-      )}
-    </SubHeaderWrapper>
-  );
+    return (
+        <SubHeaderWrapper>
+            <Title>{title}</Title>
+            {goToCart && <CartButton onClick={() => router.push("/cart")} />}
+        </SubHeaderWrapper>
+    );
 }
 
 export default SubHeader;
